4c85f53deb2973ab758d3446f80633c326f3f430
jaydipthummar123 <jaydipthummar47@gmail.com>
2025-08-08 12:00:10 +0530

    Refactor API route to include user sync functions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,12 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
+import { inngest, syncUserCreation, syncUserDeletion } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
+     syncUserCreation,
+     syncUserDeletion,
+     syncUserDeletion
   ],
 });
