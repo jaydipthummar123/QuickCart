2d6ea3c3279a1392dfaa70771f0c70e557a63b6b
jaydipthummar123 <jaydipthummar47@gmail.com>
2025-08-08 12:15:11 +0530

    Update Cloudinary environment variables and refactor API route to include user update function

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,12 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion } from "@/config/inngest";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
      syncUserCreation,
-     syncUserDeletion,
+     syncUserUpdate,
      syncUserDeletion
   ],
 });
